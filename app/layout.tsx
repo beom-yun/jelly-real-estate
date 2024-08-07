018f73185347834812e5241604aedaf7e20d4a99
beom-yun <scds0326@naver.com>
2024-08-07 22:14:09 +0900

    Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="luxury">
+      <body className={`${inter.className} mx-auto max-w-screen-sm`}>
+        {children}
+      </body>
     </html>
   );
 }
